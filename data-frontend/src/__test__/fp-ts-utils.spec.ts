0ed04be2e8cd4622fbd6bdefce0c0bfce7893110
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-11-01 21:59:50 +0200

    #23 Forgot tests.

---
+++ data-frontend/src/__test__/fp-ts-utils.spec.ts
@@ -0,0 +1,27 @@
+import test from "ava";
+import * as spec from "../fp-ts-utils";
+
+test("Validate that toEither works correctly", (c) => {
+  c.plan(3);
+  c.deepEqual(
+    spec.toEither({
+      error: "error",
+      errorInfo: undefined,
+      getHumanReadableMessage,
+    }),
+    {
+      _tag: "Left",
+      left: { error: "error", errorInfo: undefined, getHumanReadableMessage },
+    },
+  );
+  c.deepEqual(spec.toEither({ error: "error-input", errorInfo: {} }), {
+    _tag: "Left",
+    left: { error: "error-input", errorInfo: {} },
+  });
+  c.deepEqual(spec.toEither({ error: "none", data: "data" }), {
+    _tag: "Right",
+    right: "data",
+  });
+});
+
+const getHumanReadableMessage = () => "";
