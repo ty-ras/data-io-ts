7a9f591fb45152f732cfde234dcc971d0298d953
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-11-05 18:05:09 +0200

    #34 Updating dependencies of metadata-jsonschema.

--- metadata-jsonschema/src/types.d.ts
+++ metadata-jsonschema/src/types.d.ts
@@ -4,5 +4,7 @@ import type * as jsonSchema from "@ty-ras/metadata-jsonschema";
 
 export type AnyEncoder = data.Encoder<any, any>;
 export type AnyDecoder = data.Decoder<any>;
-export type FallbackValue = jsonSchema.FallbackValue<AnyEncoder | AnyDecoder>;
-export type Override = jsonSchema.Override<AnyEncoder | AnyDecoder>;
+export type FallbackValue = jsonSchema.FallbackValueGeneric<
+  AnyEncoder | AnyDecoder
+>;
+export type Override = jsonSchema.OverrideGeneric<AnyEncoder | AnyDecoder>;
